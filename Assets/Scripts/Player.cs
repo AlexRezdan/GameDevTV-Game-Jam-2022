38d46de4a487b04f0e96052bd2d3162f6a262d2f
AlexRezdan <factinfiction@yahoo.com>
2022-05-22 00:58:41 +0200

    Added more art assets and animations. Created a way to serialize different sprites (with unique animations) on the player game object.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PlayerSprites
+{
+    public Animator animator;
+    public AnimationClip[] animations;
+}
+
 public class Player : MonoBehaviour
 {
     private float horizontalInput;
@@ -14,19 +21,22 @@ public class Player : MonoBehaviour
     [Tooltip("How quickly the player falls when jump button is let go before maximum jump height.")]
     [SerializeField] private float lowJumpMultiplier = 2.0f;
 
+    [SerializeField] private PlayerSprites[] playerSprites;
+
     private bool isGrounded;
 
     private Rigidbody2D rb2d;
 
+    //[SerializeField] private Animator animator;
+    private string currentAnimation;
+    private const string IDLE = "Idle";
+    private const string WALK = "Walk";
+    private const string JUMP = "Jump";
+
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -41,6 +51,27 @@ public class Player : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector2.right * horizontalInput * moveSpeed * Time.deltaTime);
+
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        if (isGrounded)
+        {
+            if (horizontalInput != 0)
+            {
+                ChangeAnimationState(WALK);
+            }
+            else
+            {
+                ChangeAnimationState(IDLE);
+            }
+        }
     }
 
     private void Jump()
@@ -48,6 +79,11 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb2d.velocity = Vector2.up * jumpForce;
+        }
+
+        if (!isGrounded)
+        {
+            ChangeAnimationState(JUMP);
         }
 
         if (rb2d.velocity.y < 0)  // If the player is falling
@@ -74,5 +110,23 @@ public class Player : MonoBehaviour
         {
             isGrounded = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Change active child object based on what is touched.
+            // May need to create an Enum on Enemy objects to say what the player transforms into.
+        }
+    }
+
+    private void ChangeAnimationState(string newAnimation)
+    {
+        if (currentAnimation == newAnimation) return;
+
+        // TODO: Find a way for the 0 to reference the currently active child object (player sprite).
+        playerSprites[0].animator.Play(newAnimation);
+        currentAnimation = newAnimation;
     }
 }
